60b532feb82924999bb402dc5f46d0c0e3fcc94c
ivanRodrigo22 <ivan-ryan@hotmail.com>
2022-05-19 11:18:31 -0300

    add

---
+++ src/main/java/calculadora/Calculos.java
@@ -0,0 +1,15 @@
+package calculadora;
+
+public class Calculos {
+    public double soma(double... numeros){
+
+        double soma=0;
+
+        if (numeros.length>0){
+            for (double numeroASomar : numeros){
+                soma += numeroASomar;
+            }
+        }
+        return soma;
+    }
+}
